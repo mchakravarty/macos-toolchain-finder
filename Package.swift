f81eee555a890f75eac8a7dafcbd12c276e12a54
Manuel M T Chakravarty <chak@justtesting.org>
2025-04-20 21:38:05 +0200

    Set up package
    * Swift package setup
    * Use ’ArgumentParser’ to parse a language argument

---
+++ Package.swift
@@ -0,0 +1,28 @@
+// swift-tools-version: 6.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+
+  name: "macos-toolchain-finder",
+  platforms: [
+    .macOS(.v10_15)
+  ],
+
+  products: [
+    .executable(
+      name: "macos-toolchain-finder",
+    targets: ["macos-toolchain-finder"]),
+  ],
+
+  dependencies: [
+    .package(url: "https://github.com/apple/swift-argument-parser", from: "1.5.0")],
+
+  targets: [
+    .executableTarget(
+      name: "macos-toolchain-finder",
+      dependencies: [
+        .product(name: "ArgumentParser", package: "swift-argument-parser")]),
+  ]
+)
---
+++ Sources/macos-toolchain-finder.swift
@@ -0,0 +1,38 @@
+// Tool to detect language toolchains for a specific set of programming languages on macOS. Currently, three
+// languages are supported: Agda, Haskell, and Swift. More specifically, three language server protocol implementations
+// for these three languages are supported, namely Agda Language Server, Haskell Language Server, and SourceKit-LSP,
+// which supports projects using Swift, C, and C++ files.
+
+import Foundation
+import ArgumentParser
+
+
+enum Language: String, ExpressibleByArgument {
+  case agda
+  case haskell
+  case swift
+}
+
+@main
+struct MacosToolchainFinder: AsyncParsableCommand {
+
+  @Argument(help: "The programming language whose toolchains we want to find: agda, haskell, or swift.")
+  var language: Language
+
+}
+
+extension MacosToolchainFinder {
+
+  func run() async throws {
+    let toolchainFinder = MacosToolchainFinder()
+
+    switch language {
+    case .agda:
+      print("Agda")
+    case .haskell:
+      print("Haskell")
+    case .swift:
+      print("Swift")
+    }
+  }
+}
